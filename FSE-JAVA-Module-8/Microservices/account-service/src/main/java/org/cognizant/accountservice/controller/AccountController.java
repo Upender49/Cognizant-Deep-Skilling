de2fe9a1b5323253b193c2be97ae07f982a54f3b
Upender <umeshvanga05@gmail.com>
2026-07-23 21:07:28 +0530

    Micro services Added

---
+++ FSE-JAVA-Module-8/Microservices/account-service/src/main/java/org/cognizant/accountservice/controller/AccountController.java
@@ -0,0 +1,23 @@
+package org.cognizant.accountservice.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Map<String, Object> getAccount(@PathVariable String number) {
+
+        Map<String, Object> account = new HashMap<>();
+
+        account.put("number", number);
+        account.put("type", "Savings");
+        account.put("balance", 234343);
+
+        return account;
+    }
+}
